2ab0d1ae646ab4b8a55279d3badf4a6fade72ba8
Jeffrey Smith <jsmit496@stu.jccc.edu>
2018-01-29 17:39:15 -0600

    Added first person controller

---
+++ CharacterController/Assets/Scripts/FirstPersonControls.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonControls : MonoBehaviour {
+
+    public float speed = 4.0f;
+    public float motionScale = 90f;
+    public float maxAngle = 90f;
+
+    public Transform playerCamera;
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 moveDirection = Vector3.zero;
+
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            //moveDirection += new Vector3(0, 0, 1);
+
+            moveDirection += transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveDirection += -transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveDirection += -transform.right;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveDirection += transform.right;
+        }
+
+        transform.position += moveDirection * speed * Time.deltaTime;
+
+        if (mouseX > 0)
+        {
+            transform.Rotate(transform.up, motionScale * Time.deltaTime, Space.World);
+        }
+
+        if (mouseX < 0)
+        {
+            transform.Rotate(-transform.up, motionScale * Time.deltaTime, Space.World);
+        }
+
+        // Try this approach:
+        // "If I am more than (max degrees) away from dead-center,
+        // then return to dead-center view, and then
+        // rotate (max degrees) in the appropriate direction from dead-center.
+
+        if (mouseY > 0)
+        {
+            playerCamera.transform.Rotate(-transform.right, motionScale * Time.deltaTime, Space.World);
+
+            // if (Vector3.Angle(transform.forward, playerCamera.forward) > max) 
+            // {
+            //      playerCamera.transform.forward = transform.forward;
+            //      playerCamera.transform.Rotate(to the maximum distance.)
+            // }
+            if (Vector3.Angle(transform.forward, playerCamera.forward) > maxAngle)
+            {
+                playerCamera.transform.forward = transform.forward;
+                playerCamera.transform.Rotate(-transform.right, maxAngle, Space.World);
+            }
+        }
+
+        if (mouseY < 0)
+        {
+            playerCamera.transform.Rotate(transform.right, motionScale * Time.deltaTime, Space.World);
+
+            if (Vector3.Angle(transform.forward, playerCamera.forward) > maxAngle)
+            {
+                playerCamera.transform.forward = transform.forward;
+                playerCamera.transform.Rotate(transform.right, maxAngle, Space.World);
+            }
+        }
+    }
+}
---
+++ CharacterController/Assets/Scripts/MouseInputDebug.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseInputDebug : MonoBehaviour {
+
+    public Transform upBox, downBox, leftBox, rightBox;
+
+    public float motionScale = 10f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        upBox.localScale = new Vector3(1, 0.1f, 1);
+        downBox.localScale = new Vector3(1, 0.1f, 1);
+        leftBox.localScale = new Vector3(1, 0.1f, 1);
+        rightBox.localScale = new Vector3(1, 0.1f, 1);
+
+        if (mouseX > 0)
+        {
+            // Scale the right box
+            rightBox.localScale = new Vector3(1, mouseX * motionScale, 1);
+        }
+        
+        if (mouseX < 0)
+        {
+            // Scale the left box
+            leftBox.localScale = new Vector3(1, mouseX * motionScale, 1);
+        }
+
+        if (mouseY > 0)
+        {
+            upBox.localScale = new Vector3(1, mouseY * motionScale, 1);
+        }
+
+        if (mouseY < 0)
+        {
+            downBox.localScale = new Vector3(1, mouseY * motionScale, 1);
+        }
+
+    }
+}
